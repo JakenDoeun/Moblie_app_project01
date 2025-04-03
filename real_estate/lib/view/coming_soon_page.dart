a7c9ae1ec7ddab7e4a5177d0affe77c8a0a8f6b4
Narin-Chhorn <esaranarin.chhorn@student.cadt.edu.kh>
2025-04-04 00:56:07 +0700

    new file

---
+++ real_estate/lib/view/coming_soon_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(ComingSoonApp());
+}
+
+class ComingSoonApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Coming Soon',
+      theme: ThemeData.light(),
+      home: ComingSoonPage(),
+    );
+  }
+}
+
+class ComingSoonPage extends StatefulWidget {
+  @override
+  _ComingSoonPageState createState() => _ComingSoonPageState();
+}
+
+class _ComingSoonPageState extends State<ComingSoonPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeInAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: Duration(seconds: 2));
+    _fadeInAnimation = Tween<double>(begin: 0, end: 1).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background with gradient and abstract shape
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.white, Colors.grey[200]!],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+
+          // Subtle geometric pattern
+          Positioned(
+            right: -100,
+            top: -100,
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue.withOpacity(0.1),
+              ),
+            ),
+          ),
+
+          // Main Content
+          Center(
+            child: FadeTransition(
+              opacity: _fadeInAnimation,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // "COMING SOON" Text with soft glow
+                  Text(
+                    "COMING SOON",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 42,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2,
+                      color: Colors.blueAccent,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10,
+                          color: Colors.blueAccent.withOpacity(0.6),
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16),
+
+                  // Brief description
+                  Text(
+                    "Something amazing is on the way! Please be Patient!",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      color: Colors.grey[600],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
